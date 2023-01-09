4bee7221ad63480b1c51c3f6f78f65b968535c30
ScaraMouche <114559016+Leviachann@users.noreply.github.com>
2023-01-09 11:12:16 +0400

    Quiz app
    mellim windows forms hele bele gorsenir duszu ama menim ilk yazdigim gui desktop app di diye (python tkinterden basqa) cox problem cixirdi 5-6 saat vaxtimi aldi umidvaramki  beyenrsiniz

---
+++ QuizGame/QuizGame/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace QuizGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LblQuestion = new System.Windows.Forms.Label();
+            this.Ans2 = new System.Windows.Forms.Button();
+            this.Ans1 = new System.Windows.Forms.Button();
+            this.Ans3 = new System.Windows.Forms.Button();
+            this.Ans4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuizGame.Properties.Resources.genshinimpact;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 321);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LblQuestion
+            // 
+            this.LblQuestion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LblQuestion.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblQuestion.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LblQuestion.Location = new System.Drawing.Point(26, 370);
+            this.LblQuestion.Name = "LblQuestion";
+            this.LblQuestion.Size = new System.Drawing.Size(750, 41);
+            this.LblQuestion.TabIndex = 1;
+            this.LblQuestion.Text = "Question";
+            this.LblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Ans2
+            // 
+            this.Ans2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ans2.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ans2.Location = new System.Drawing.Point(408, 432);
+            this.Ans2.Name = "Ans2";
+            this.Ans2.Size = new System.Drawing.Size(368, 43);
+            this.Ans2.TabIndex = 3;
+            this.Ans2.Tag = "2";
+            this.Ans2.Text = "Answer 2";
+            this.Ans2.UseVisualStyleBackColor = false;
+            this.Ans2.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // Ans1
+            // 
+            this.Ans1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ans1.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ans1.Location = new System.Drawing.Point(26, 432);
+            this.Ans1.Name = "Ans1";
+            this.Ans1.Size = new System.Drawing.Size(368, 43);
+            this.Ans1.TabIndex = 4;
+            this.Ans1.Tag = "1";
+            this.Ans1.Text = "Answer 1";
+            this.Ans1.UseVisualStyleBackColor = false;
+            this.Ans1.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // Ans3
+            // 
+            this.Ans3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ans3.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ans3.Location = new System.Drawing.Point(26, 481);
+            this.Ans3.Name = "Ans3";
+            this.Ans3.Size = new System.Drawing.Size(368, 43);
+            this.Ans3.TabIndex = 5;
+            this.Ans3.Tag = "3";
+            this.Ans3.Text = "Answer 3";
+            this.Ans3.UseVisualStyleBackColor = false;
+            this.Ans3.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // Ans4
+            // 
+            this.Ans4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ans4.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Ans4.Location = new System.Drawing.Point(408, 481);
+            this.Ans4.Name = "Ans4";
+            this.Ans4.Size = new System.Drawing.Size(368, 43);
+            this.Ans4.TabIndex = 6;
+            this.Ans4.Tag = "4";
+            this.Ans4.Text = "Answer 4";
+            this.Ans4.UseVisualStyleBackColor = false;
+            this.Ans4.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.Ans4);
+            this.Controls.Add(this.Ans3);
+            this.Controls.Add(this.Ans1);
+            this.Controls.Add(this.Ans2);
+            this.Controls.Add(this.LblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LblQuestion;
+        private System.Windows.Forms.Button Ans2;
+        private System.Windows.Forms.Button Ans1;
+        private System.Windows.Forms.Button Ans3;
+        private System.Windows.Forms.Button Ans4;
+    }
+}
+
---
+++ QuizGame/QuizGame/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizGame
+{
+    public partial class Form1 : Form
+    {
+        //variables
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int rightAnswers;
+        int totalQuestions=10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AskQuestion(questionNumber);
+        }
+        
+        private void CheckAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject=(Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+            var rightans = (Button)sender;
+            if (buttonTag == correctAnswer)
+            {
+                score += 10;
+                rightAnswers+=1;
+            }
+            else
+            {
+                
+                if ((score-10)> 0) { score -= 10; }
+                if (Convert.ToInt32(Ans1.Tag) == correctAnswer)
+                {
+                    rightans = Ans1;
+                }
+                else if (Convert.ToInt32(Ans2.Tag) == correctAnswer)
+                {
+                 
+                    rightans = Ans2;
+                }
+                else if (Convert.ToInt32(Ans3.Tag) == correctAnswer)
+                {
+                 
+                    rightans = Ans3;
+                }
+                else if (Convert.ToInt32(Ans4.Tag) == correctAnswer)
+                {
+                    
+                    rightans = Ans4;
+                }
+            }
+            if (questionNumber == 10)
+            {
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + rightAnswers + " questions correctly." + Environment.NewLine +
+                    "You're score is " + score  + Environment.NewLine +
+                    "Click OK to play again"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                AskQuestion(questionNumber);
+            }
+            LblQuestion.Text="Next Question..";
+            TestCorrect();
+            async Task TestCorrect()
+            {
+                await Task.Run(async () =>
+                {
+                    rightans.BackColor = Color.Green;
+                    if (rightans != senderObject) {
+                        senderObject.BackColor = Color.Red;
+                    }
+                    await Task.Delay(300);
+                    senderObject.BackColor = Color.White;
+                    rightans.BackColor = Color.White;
+                });
+                Console.WriteLine("All done");
+            }
+            
+            questionNumber++;
+            AskQuestion(questionNumber);
+            
+
+        }
+
+        private void AskQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.genshinimpact;
+                    LblQuestion.Text = "What is the name of this game?";
+                    Ans1.Text = "Genshin Impact";
+                    Ans2.Text = "Grand Theft Auto V";
+                    Ans3.Text = "Legend Of Zelda: Breath of the wild";
+                    Ans4.Text = "Tower of Fantasy";
+                    correctAnswer = 1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.dragonspine;
+                    LblQuestion.Text = "Which region was the first one released?";
+                    Ans1.Text = "Sumeru";
+                    Ans2.Text = "Liyue";
+                    Ans3.Text = "Inazuma";
+                    Ans4.Text = "Mondstadt";
+                    correctAnswer = 4;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.sumeru;
+                    LblQuestion.Text = "What is the name of this region?";
+                    Ans1.Text = "Teyvat";
+                    Ans2.Text = "Fontaine";
+                    Ans3.Text = "Sumeru";
+                    Ans4.Text = "Dragonspine";
+                    correctAnswer = 3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.rexlapis;
+                    LblQuestion.Text = "What is the name of the Geo Archon?";
+                    Ans1.Text = "Paimon";
+                    Ans2.Text = "Rex Lapis";
+                    Ans3.Text = "Venti";
+                    Ans4.Text = "Raiden Shogun";
+                    correctAnswer = 2;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.cryo;
+                    LblQuestion.Text = "What element does she use?";
+                    Ans1.Text = "Pyro";
+                    Ans2.Text = "Cryo";
+                    Ans3.Text = "Anemo";
+                    Ans4.Text = "Hydro";
+                    correctAnswer = 2;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.zombie;
+                    LblQuestion.Text = "What creature is she?";
+                    Ans1.Text = "Archon";
+                    Ans2.Text = "Adeptus";
+                    Ans3.Text = "Zombie";
+                    Ans4.Text = "Fatui";
+                    correctAnswer = 3;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.hydroabyssmage;
+                    LblQuestion.Text = "What is the name of the enemy?";
+                    Ans1.Text = "Matachurl";
+                    Ans2.Text = "Hilichurl";
+                    Ans3.Text = "Ruin Guard";
+                    Ans4.Text = "Hydro Abyss Mage";
+                    correctAnswer = 4;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.faruzan;
+                    LblQuestion.Text = "When was she released?";
+                    Ans1.Text = "3.3";
+                    Ans2.Text = "4.2";
+                    Ans3.Text = "2.6";
+                    Ans4.Text = "1.5";
+                    correctAnswer = 1;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources.jean;
+                    LblQuestion.Text = "What is her sister's name?";
+                    Ans1.Text = "Barbara";
+                    Ans2.Text = "Jean";
+                    Ans3.Text = "Wanderer";
+                    Ans4.Text = "Lumine";
+                    correctAnswer = 2;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources.fromashesreborn;
+                    LblQuestion.Text = "What was his featured banner name?";
+                    Ans1.Text = "From Ashes Reborn";
+                    Ans2.Text = "Azure Excursion";
+                    Ans3.Text = "Reign of Serenity";
+                    Ans4.Text = "Leaves in the wind";
+                    correctAnswer = 1;
+                    break;
+
+            }
+
+
+        }
+    }
+}
